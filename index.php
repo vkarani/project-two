d6090a5a411c5dfe6179d7128c92e3b94d06dbff
Victor Karani <victor.karani@gmail.com>
2014-10-02 17:30:24 -0400

    Can now set the number of words used for a passwd between 1-9

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <!DOCTYPE html>
+<?php
+error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+?>
+
 <html>
   <head>
   <meta charset="UTF-8">
@@ -10,20 +15,25 @@
    <div class="container">
      <h1> Victor Karani P2 <br> XKCD Password generator</h1>
        <p class="password">
-         <?php  for($i = 0; $i < 5; $i++): ?>  <!-- REPLACE 5 with Number of iterations from the form -->
+         
+         <?php  for($i = 0; $i < $iterations; $i++): ?>
           <?php $current_word=array_rand($word_list);?> 
           <?=$word_list[$current_word]?>
-          <?php  if($i<4): ?>
+          <?php  if($i < ($iterations -1)): ?>
            -
           <?php endif; ?>        
          <?php endfor; ?>    
        </p>
        <form method="GET" action="index.php">
          <label for='number_of_words'># of Words</label>  <!-- Make this more original?-->
-         <input maxlength=1 type='text' name='number_of_words' id='number_of_words' value='5'>  (Max 9)
+         <input maxlength=1 type='text' name='number_of_words' id='number_of_words' value= <?=$iterations?> >  (Max 9)
          <br>	
          <input type='submit' class='btn btn-default' value='Create Another Password'>
        </form>
    </div>   
+   <pre>
+    <?php print_r($_GET); ?>
+    <?php print_r($iterations); ?>
+   </pre>
   </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,4 +1,11 @@
 <?php
+$iterations=5;
+if(! empty ($_GET)) {
+  $iterations = $_GET[number_of_words];
+}
+if(($iterations < 1) ||( $iterations > 10)) {
+	$iterations=5;
+}
 
 $re = "/\\<li\\>[\\n, ,\\t,\\r]*[a-z]*[\\n, ,\\t,\\r]*\\<\\/li\\>/"; //regular expression, get all between the <li></li> 
 $testfile = file_get_contents('http://www.paulnoll.com/Books/Clear-English/words-29-30-hundred.html');
@@ -9,3 +16,5 @@ foreach($word_list as &$value){
 	$value=str_replace("</li>", "", $value);
 }
  $current_word='';
+ 
+ 
