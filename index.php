ddc2529dc52201b4cb0a601502eec0dbb650ee1b
Victor Karani <victor.karani@gmail.com>
2014-10-02 23:12:01 -0400

    Can now add rudimentary symbol and number

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<?php
-error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
-ini_set('display_errors', 1); # Display errors on page (instead of a log file)
-?>
-
 <html>
   <head>
   <meta charset="UTF-8">
@@ -14,26 +8,37 @@ ini_set('display_errors', 1); # Display errors on page (instead of a log file)
   <body>
    <div class="container">
      <h1> Victor Karani P2 <br> XKCD Password generator</h1>
-       <p class="password">
-         
+       <p class="password">  
          <?php  for($i = 0; $i < $iterations; $i++): ?>
           <?php $current_word=array_rand($word_list);?> 
           <?=$word_list[$current_word]?>
           <?php  if($i < ($iterations -1)): ?>
            -
           <?php endif; ?>        
-         <?php endfor; ?>    
+         <?php endfor; ?> 
+         <?php if($use_number==true): ?>
+			9
+			<?php $display_number="checked"; endif; ?>
+         <?php if($use_symbol==true): ?>
+			&
+			<?php $display_symbol="checked"; endif; ?>
        </p>
        <form method="GET" action="index.php">
          <label for='number_of_words'># of Words</label>  <!-- Make this more original?-->
-         <input maxlength=1 type='text' name='number_of_words' id='number_of_words' value= <?=$iterations?> >  (Max 9)
+         <input maxlength=1 type='text' name='number_of_words' id='number_of_words' value= <?=$iterations?> >  (Maximum 9)
          <br>	
+         
+         <input type='checkbox' name='add_number' id='add_number' <?=$display_number?> > 
+			<label for='add_number'>Add a number</label>
+			<br>
+			
+			
+			<input type='checkbox' name='add_symbol' id='add_symbol'<?=$display_symbol?>  > 
+			<label for='add_symbol'>Add a symbol</label>
+			<br>        
+         
          <input type='submit' class='btn btn-default' value='Create Another Password'>
        </form>
    </div>   
-   <pre>
-    <?php print_r($_GET); ?>
-    <?php print_r($iterations); ?>
-   </pre>
   </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,7 +1,19 @@
 <?php
 $iterations=5;
+$use_number=false;
+$display_number="";
+$use_symbol=false;
+$display_symbol="";
 if(! empty ($_GET)) {
   $iterations = $_GET[number_of_words];
+  if(isset($_GET[add_number])) {
+  	$use_number=true;
+  }
+  if(isset($_GET[add_symbol])) {
+  	$use_symbol=true;
+  }
+  //$use_number = ;
+  //$use_symbol = ;
 }
 if(($iterations < 1) ||( $iterations > 10)) {
 	$iterations=5;
@@ -14,6 +26,9 @@ $word_list=$match[0];
 foreach($word_list as &$value){
 	$value=str_replace("<li>", "",$value);
 	$value=str_replace("</li>", "", $value);
+	$value=str_replace("\n", "", $value);
+	$value=str_replace("\t", "", $value);
+	$value=str_replace("\r", "", $value);
 }
  $current_word='';
  
